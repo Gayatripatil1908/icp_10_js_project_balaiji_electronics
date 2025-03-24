d1625304452270bf78712df1e39b097c840ab0ac
shubhangi-raskar6060 <shubhangi1753@gmail.com>
2025-03-24 13:05:03 +0530

    script added

---
+++ js/blog.js/video.js
@@ -0,0 +1,32 @@
+
+        const mediaContainer = document.querySelector('.media-container');
+const mediaItems = [
+  {
+    type: 'image',
+    src: 'https://via.placeholder.com/300',
+    alt: 'Image 1',
+  },
+  {
+    type: 'video',
+    src: 'https://www.youtube.com/embed/VIDEO_ID',
+    title: 'Video 1',
+  },
+  {
+    type: 'infographic',
+    src: 'https://via.placeholder.com/300',
+    alt: 'Infographic 1',
+  },
+];
+
+mediaItems.forEach((mediaItem) => {
+  const mediaElement = document.createElement(mediaItem.type === 'video' ? 'iframe' : 'img');
+  mediaElement.src = mediaItem.src;
+  mediaElement.alt = mediaItem.alt;
+  mediaElement.title = mediaItem.title;
+  const mediaItemElement = document.createElement('div');
+  mediaItemElement.classList.add('media-item');
+  mediaItemElement.appendChild(mediaElement);
+  mediaContainer.appendChild(mediaItemElement);
+});
+
+      
